9891af7e65ba1d031d08f263db7a5b16d34c8647
Doug Bell <madcityzen@gmail.com>
2014-11-13 23:42:47 -0600

    introduce array slices before hashy slicest

--- 5.20/postderef.pl
+++ 5.20/postderef.pl
@@ -8,26 +8,29 @@ my $href = {
 };
 my $aref = [ "foo", "bar", "baz", "fuzz" ];
 
-say "-- Circumfix hash dereference";
-say %{ $href };
-
-say "-- Postfix hash dereference";
-say $href->%*;
-
 say "-- Circumfix array dereference";
 say @{ $aref };
 
 say "-- Postfix array dereference";
 say $aref->@*;
 
-say "-- Circumfix hash slice";
-say %{ $href }{ 'soda', 'foo' };
+say "-- Circumfix hash dereference";
+say %{ $href };
 
-say "-- Postfix hash slice";
-say $href->%{ 'soda', 'foo' };
+say "-- Postfix hash dereference";
+say $href->%*;
 
 say "-- Circumfix array slice";
 say @{ $aref }[ 1..2 ];
 
 say "-- Postfix array slice";
 say $aref->@[ 1..2 ];
+
+say "-- Circumfix hash slice";
+say %{ $href }{ 'soda', 'foo' };
+
+say "-- Postfix hash array-slice";
+say $href->@{ 'soda', 'foo' };
+
+say "-- Postfix hash hash-slice";
+say $href->%{ 'soda', 'foo' };
